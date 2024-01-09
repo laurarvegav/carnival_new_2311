4d95d957fd3c30be5fc408d188aa93c1165a5ebf
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-09 11:40:20 -0700

    Test: add test for ride_summary method for Ride class

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -98,4 +98,27 @@ RSpec.describe Ride do
             expect(ride3.total_riders).to eq(1)
         end
     end
+
+    describe "#summary" do
+        it "can return complete summary" do
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+            visitor3 = Visitor.new('Penny', 64, '$15')
+            visitor1.add_preference(:gentle)
+            visitor2.add_preference(:gentle)
+            visitor2.add_preference(:thrilling)
+            visitor3.add_preference(:thrilling)
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor2)
+            ride1.board_rider(visitor1)
+            ride3.board_rider(visitor1)
+            ride3.board_rider(visitor2)
+            ride3.board_rider(visitor3)
+
+            expect(ride1.ride_summary).to eq({ride: ride1, riders: [visitor1, visitor2], total_revenue: 3})
+        end
+    end
 end
