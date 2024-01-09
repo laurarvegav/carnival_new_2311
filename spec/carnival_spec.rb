e505706e1e20365745de357b7e067d18062ce017
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-09 10:39:19 -0700

    Test: add tests to return correct values for total_revenue, most_profitable_ride and most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,1 +1,121 @@
 require 'spec_helper'
+
+RSpec.describe Carnival do
+    describe "#initialize" do
+        it "exists as a carnival" do
+            carnival1 = Carnival.new('Broomfield', "2 weeks")
+
+            expect(carnival1).to be_a Carnival
+        end
+        
+        it "has a duration" do
+            carnival1 = Carnival.new('Broomfield', 2)
+
+            expect(carnival1.duration).to eq(14)
+        end
+    end
+
+    describe "#add_ride" do
+        it "starts with no rides" do
+            carnival1 = Carnival.new('Broomfield', 2)
+
+            expect(carnival1.rides).to eq([])
+        end
+        it "add rides and can return an array of ride objects" do
+            carnival1 = Carnival.new('Broomfield', 2)
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+            carnival1.add_ride(ride1)
+            carnival1.add_ride(ride2)
+            carnival1.add_ride(ride3)
+
+            expect(carnival1.rides).to eq([ride1, ride2, ride3])
+        end
+    end
+
+    describe "#most_popular_ride" do
+        it "returns the most popular ride" do
+            carnival1 = Carnival.new('Broomfield', 2)
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+            visitor3 = Visitor.new('Penny', 64, '$15')
+            visitor1.add_preference(:gentle)
+            visitor2.add_preference(:gentle)
+            visitor2.add_preference(:thrilling)
+            visitor3.add_preference(:thrilling)
+            carnival1.add_ride(ride1)
+            carnival1.add_ride(ride2)
+            carnival1.add_ride(ride3)
+
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor3)
+            ride1.board_rider(visitor1)
+            ride3.board_rider(visitor2)
+            ride3.board_rider(visitor3)
+
+            expect(carnival1.most_popular_ride).to eq(ride1)
+        end
+    end
+
+    describe "#most_profitable_ride" do
+        it "returns the most profitable ride" do
+            carnival1 = Carnival.new('Broomfield', 2)
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+            visitor3 = Visitor.new('Penny', 64, '$15')
+            visitor1.add_preference(:gentle)
+            visitor2.add_preference(:gentle)
+            visitor2.add_preference(:thrilling)
+            visitor3.add_preference(:thrilling)
+            carnival1.add_ride(ride1)
+            carnival1.add_ride(ride2)
+            carnival1.add_ride(ride3)
+
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor3)
+            ride1.board_rider(visitor1)
+            ride2.board_rider(visitor1)
+            ride3.board_rider(visitor2)
+            ride3.board_rider(visitor3)
+
+            expect(carnival1.most_profitable_ride).to eq(ride2)
+        end
+    end
+
+    describe "#total_revenue" do
+        it "returns correctly total revenue" do
+            carnival1 = Carnival.new('Broomfield', 2)
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 55, '$5')
+            visitor3 = Visitor.new('Penny', 64, '$15')
+            visitor1.add_preference(:gentle)
+            visitor2.add_preference(:gentle)
+            visitor2.add_preference(:thrilling)
+            visitor3.add_preference(:gentle)
+            visitor3.add_preference(:thrilling)
+            carnival1.add_ride(ride1)
+            carnival1.add_ride(ride2)
+            carnival1.add_ride(ride3)
+
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor3)
+            ride1.board_rider(visitor1)
+            ride2.board_rider(visitor1)
+            ride3.board_rider(visitor2)
+            ride3.board_rider(visitor3)
+
+            expect(carnival1.total_revenue).to eq(12)
+        end
+    end
+end
